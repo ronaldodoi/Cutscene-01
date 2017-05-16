8603bb8143e82ef4cdd1b5d7f9a82c4eeccd7dfa
Arthur Pereira <arthurpolima@gmail.com>
2017-05-16 12:21:25 -0300

    Programando o corte entre as cameras

---
+++ Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneController : MonoBehaviour {
+
+    public static CutsceneController instacia = null;
+    public List<GameObject> cameras = new List<GameObject>();
+    int ativa;
+
+    void Start() {
+        if (instacia == null)
+        {
+            instacia = this;
+        }
+
+        for (int i=0; i<cameras.Count; i++)
+        {
+            cameras[i].SetActive(false);
+        }
+        cameras[0].SetActive(true);
+        ativa = 0;
+    }
+
+    public void ProximaCamera()
+    {
+        cameras[ativa].SetActive(false);
+        if (ativa < cameras.Count - 1)
+        {
+            ativa++;
+            cameras[ativa].SetActive(true);
+        }
+    }
+}
+
---
+++ Assets/Scripts/FimDeCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FimDeCamera : MonoBehaviour {
+
+	public void CameraAcabou()
+    {
+        CutsceneController.instacia.ProximaCamera();
+    }
+
+}
